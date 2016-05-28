e1bb7f9c73d5bea5b3d177e80aa2e6ee41653380
ELLIOTTCABLE <me@ell.io>
2016-05-28 11:05:21 -0400

    + using npm-eval

--- giraphe.es6.js
+++ giraphe.es6.js
@@ -4,18 +4,19 @@ import assert from 'power-assert'
 
 import _ from 'lodash'
 import Mustache from 'mustache'
+import require_eval from 'eval'
 import { resolve } from 'path'
 import { readFileSync } from 'fs'
 
 // XXX: Does this need to be instantiated?
 Mustache.escape = function(s){ return s }
 
-const walkTemplate = readFileSync(resolve(__dirname, 'walk.js.mustache'), 'utf8')
-Mustache.parse(walkTemplate)
+let walkTemplate = readFileSync(resolve(__dirname, 'walk.js.mustache'), 'utf8')
 assert(typeof walkTemplate === 'string' && 0 !== walkTemplate.length)
+process.nextTick(()=> Mustache.parse(walkTemplate))
 
 
-var Walker = function(options) {
+var Walker = function(options, key) {
 
    if (_.isFunction(options))
       options = { class: options }
@@ -54,10 +55,10 @@ const constructWalkFunction = function(options){
    const body = Mustache.render(walkTemplate, options)
    assert(typeof body === 'string' && 0 !== body.length)
 
-   const wrap = (null,eval)(body)
+   const wrap = require_eval(body, true).default
    assert(typeof wrap === 'function')
 
-   const func = wrap(debug, assert, _, symbols, options)
+   const func = wrap(symbols, options)
 
    return function invokeWalk(root, ...callbacks){
       // If method-invoked, `root.walk(...)`
